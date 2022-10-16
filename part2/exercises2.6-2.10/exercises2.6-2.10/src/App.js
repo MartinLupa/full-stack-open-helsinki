7ed6d6cc152285979fa9446a5b7052184e2cef7c
Martin Lupa <martinignaciolupa@gmail.com>
2022-10-16 17:12:48 +0200

    part2 - excercise 2.9, name filter added

--- part2/exercises2.6-2.10/exercises2.6-2.10/src/App.js
+++ part2/exercises2.6-2.10/exercises2.6-2.10/src/App.js
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const phonebookInitialState = [
+  { name: "Arto Hellas", number: "040-123456", id: 1 },
+  { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+  { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+  { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+];
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState(phonebookInitialState);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
 
   const handleNameInput = (event) => {
     setNewName(event.target.value);
@@ -36,12 +48,31 @@ const App = () => {
     }
   };
 
+  useEffect(() => {
+    if (filter.length === 0) {
+      setPersons(phonebookInitialState);
+    } else {
+      const filteredPersons = persons.filter((person) =>
+        person.name.includes(filter)
+      );
+      setPersons(filteredPersons);
+    }
+  }, [filter]);
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+
+      <p>
+        Filter shown with{" "}
+        <input type="text" onChange={handleFilterChange} value={filter} />
+      </p>
+
+      <h2>Add new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input onChange={handleNameInput} value={newName} />
+          <br />
           number: <input onChange={handleNumberInput} value={newNumber} />
         </div>
         <div>
@@ -51,7 +82,9 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>
+            {person.name} - {person.number}
+          </li>
         ))}
       </ul>
     </div>
