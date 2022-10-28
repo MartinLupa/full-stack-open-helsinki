a849e283ebde650a1d9b59d9c2207fdc2b9f74cf
Martin Lupa <martinignaciolupa@gmail.com>
2022-10-28 15:06:43 +0200

    part3 - excercise 3.8, morgan custom log with POST req content

--- part3/exercises3.1-3.8/index.js
+++ part3/exercises3.1-3.8/index.js
@@ -25,9 +25,21 @@ let persons = [
   },
 ];
 
+//Morgan logger configuration
+morgan.token("body", function (req, res) {
+  const requestMethod = req.method;
+  if (requestMethod === "POST") {
+    const reqBody = JSON.stringify(req.body);
+    return reqBody;
+  }
+});
+
 //Middlewares
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 //GET all persons
 app.get("/api/persons", (req, res) => {
