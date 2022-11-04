be07b3cabb004a4f2605c361959662c2811e7b3e
Martin Lupa <martinignaciolupa@gmail.com>
2022-11-04 18:34:31 +0100

    part3 - exercise 3.22, eslint added to projects and errors fixed

--- part3/exercises3.1-3.21/index.js
+++ part3/exercises3.1-3.21/index.js
@@ -1,38 +1,38 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const cors = require("cors");
-const Person = require("./models/person");
-const { response } = require("express");
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+const cors = require("cors")
+const Person = require("./models/person")
+const { response } = require("express")
 
 //Morgan logger configuration
-morgan.token("body", function (req, res) {
-  const requestMethod = req.method;
+morgan.token("body", function (req) {
+  const requestMethod = req.method
   if (requestMethod === "POST") {
-    const reqBody = JSON.stringify(req.body);
-    return reqBody;
+    const reqBody = JSON.stringify(req.body)
+    return reqBody
   }
-});
+})
 
 //Middlewares
-app.use(cors());
-app.use(express.json());
-app.use(express.static("build"));
+app.use(cors())
+app.use(express.json())
+app.use(express.static("build"))
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+)
 
 //GET all persons
 app.get("/api/persons", (req, res) => {
   Person.find({}).then((persons) => {
-    res.status(200).json(persons);
-  });
-});
+    res.status(200).json(persons)
+  })
+})
 
 //GET info
-app.get("/info", (req, res) => {
-  const phonebookLength = persons.length;
+app.get("/info", async (req, res) => {
+  const phonebookLength = await Person.countDocuments({}).exec()
   const infoDisplay = `<div>
                         <p>
                         Phonebook has info for ${phonebookLength} people
@@ -40,79 +40,79 @@ app.get("/info", (req, res) => {
                         <p>
                         ${new Date()}
                         </p>
-                      </div>`;
+                      </div>`
 
-  res.status(200).send(infoDisplay);
-});
+  res.status(200).send(infoDisplay)
+})
 
 //GET person by id
 app.get("/api/persons/:id", (req, res, next) => {
   Person.findById(req.params.id)
     .then((persons) => {
       if (persons) {
-        response.json(persons);
+        response.json(persons)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 //DELETE person by id
 app.delete("/api/persons/:id", (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then((persons) => {
       if (persons) {
-        response.json(persons);
+        response.json(persons)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 //POST new person
 app.post("/api/persons", (req, res, next) => {
-  const { name, number } = req.body;
+  const { name, number } = req.body
 
-  if (!name || !number || persons.find((person) => person.name === name)) {
+  if (!name || !number) {
     res.status(400).json({
       data: null,
       error: "name and number cannot be empty, and name must be unique",
-    });
+    })
   }
 
   const person = new Person({
     name,
     number,
-  });
+  })
 
   person
     .save()
     .then((savedPerson) => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
     .catch((error) => {
-      next(error);
-    });
-});
+      next(error)
+    })
+})
 
 //Error handler
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
   if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
+    return response.status(400).send({ error: "malformatted id" })
   }
 
-  next(error);
-};
+  next(error)
+}
 
 // this has to be the last loaded middleware.
-app.use(errorHandler);
+app.use(errorHandler)
 
 //App listening
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Backend listening on port ${PORT}`);
-});
+  console.log(`Backend listening on port ${PORT}`)
+})
--- part3/exercises3.1-3.21/models/person.js
+++ part3/exercises3.1-3.21/models/person.js
@@ -1,16 +1,16 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
-const connection_url = process.env.DATABASE_CONNECTION_URL;
+require("dotenv").config()
+const mongoose = require("mongoose")
+const connection_url = process.env.DATABASE_CONNECTION_URL
 
-console.log("connecting to database");
+console.log("connecting to database")
 mongoose
   .connect(connection_url)
-  .then((result) => {
-    console.log("database connection successfully");
+  .then(() => {
+    console.log("database connection successfully")
   })
   .catch((error) => {
-    console.log("database connection error: ", error);
-  });
+    console.log("database connection error: ", error)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -18,15 +18,15 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
   },
   number: String,
-});
+})
 
 //Deletes _id and __v from the object sent to the frontend, and transforms to String the Mongo id Object.
 personSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model("Person", personSchema)
